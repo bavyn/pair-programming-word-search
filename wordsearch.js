31bca32fb45c01240b3210848141facd6b94a3de
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-05-10 17:11:50 -0700

    pair programming word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,26 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''))
 
+  for (l of horizontalJoin) {
+      if (l.includes(word)) {
+          return true;
+      }
+
+  }
+  const verticalJoin = []
+  for (let col = 0; col < letters[0].length; col++) {
+      let vertical;
+      for (let row = 0; row < letters[0].length; row++) {
+          vertical += letters[row][col];
+      }
+      verticalJoin.push(vertical);
+  }
+
+  for (let row of verticalJoin) {
+      if (row.includes(word)) {
+          return true;
+      }
+  }
+  return false;
+};
 module.exports = wordSearch
